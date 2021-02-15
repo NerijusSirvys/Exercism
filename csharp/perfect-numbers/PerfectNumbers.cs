6dcea57b73a89b3d7e684da8f94548ad965ba233
nerijus.sirvys <nerijus.sirvys@gmail.com>
2021-02-15 18:41:20 +0000

    PerfectNumbers--solution

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number <= 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        int sum = 0;
+
+        for (int i = 1; i < number; i++)
+        {
+            sum += GetDivisor(number, i);
+        }
+
+        if (sum == number)
+        {
+            return Classification.Perfect;
+        }
+
+        if (sum < number)
+        {
+            return Classification.Deficient;
+        }
+
+        return Classification.Abundant;
+
+    }
+
+    private static int GetDivisor(int number, int i)
+    {
+        if (number % i == 0)
+        {
+            return i;
+        }
+
+        return 0;
+    }
+}
